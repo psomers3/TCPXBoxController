ce7d8dfa558f9ab5a4de290ce12f109a983cffd6
ac133451 <peter.somers@isys.uni-stuttgart.de>
2020-09-17 15:38:49 +0200

    updated default ports

--- TCPXboxController/__main__.py
+++ TCPXboxController/__main__.py
@@ -3,8 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Connect to an Xbox 360 controller and forward the signals on a TCP port')
 parser.add_argument('-D', '--dev_num', type=int, help='device number corresponding to desired controller', default=0)
-parser.add_argument('-S', '--send_port', type=int, help='TCP port to send the signals to. Default=36363', default=36363)
-parser.add_argument('-R', '--receive_port', type=int, help='TCP port to receive vibration commands. Default=36364', default=36364)
+parser.add_argument('-S', '--send_port', type=int, help='TCP port to send the signals to. Default=63636', default=63636)
+parser.add_argument('-R', '--receive_port', type=int, help='TCP port to receive vibration commands. Default=63637', default=63637)
 parser.add_argument('-I', '--ip_addr', type=str, help='ip address to use for TCP communication. Default=\'localhost\'', default='localhost')
 
 if __name__ == '__main__':
